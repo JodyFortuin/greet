9b3977a1db4859b0c0247daa1f3184a9782551f0
JodyFortuin <60154410+JodyFortuin@users.noreply.github.com>
2020-06-23 21:55:28 +0200

    commit

--- greet.js
+++ greet.js
@@ -1,20 +1,14 @@
+ /* const greetFact = greetFunction(nameMap);*/
 const nameElem = document.querySelector(".nameEntered");
 const languageElem = document.querySelector(".languageRadio");
 const greetBtn = document.querySelector(".greetBtn");
 const greetCounterElem = document.querySelector(".greetCounter");
 
-function greet(name) {
-    var nameEnt = name.split(',');
-    var greetTotal = 0;
-    for(var i=0; i < nameEnt.length; i++){
-      var names = nameEnt[i].trim();
-        if (names == String){
-            greetTotal += 1;
-         }
-    }
-  var greeting = "Hello, " + name;
-  return greeting;
-}
+const namesInLocalStorage = localStorage['namesListed'];
+  let nameMap = null;
+  if (namesInLocalStorage) {
+	nameMap = JSON.parse(namesInLocalStorage);
+  }
 
 function greetBtnClicked(){
     var checkedRadioBtn = document.querySelector("input[name='langItem']:checked");
@@ -22,7 +16,18 @@ function greetBtnClicked(){
     var name = nameElem.value;
     const greeting = greet(name);
     greetCounterElem.innerHTML = greetTotal;
-    
 }
 
-greetBtn.addEventListener('click', greetBtnClicked); 
+var greetTotal = 0;
+greetBtn.addEventListener('click', function(){
+    greetTotal++;
+    let name;
+	  if (nameElem && nameElem.value) {
+      name = nameEntered.value;
+	  } else {
+		return;
+    }
+    /*message.innerHTML = greetFact.greet(name);*/
+  
+    localStorage['namesListed'] = JSON.stringify(greetFact.namesListed());
+});
