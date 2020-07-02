f5ec485421b67df10d66e50334da9659babf3f4d
JodyFortuin <60154410+JodyFortuin@users.noreply.github.com>
2020-07-02 20:55:42 +0200

    Update greet.factory.js

--- greet.factory.js
+++ greet.factory.js
@@ -1,27 +1,57 @@
-function greetFact(savedState){
-  
+function greetFactory(savedState) {
 
-const namesListedMap = savedState || document.createElement("li");
-li.innerHTML = nameElem.value;
-var t = document.createTextNode("li");
-name.appendChild(t);
-nameElem.value = '';
+    var namesListedMap = savedState || {};
 
-
-function greet(name) {
-    if (namesListedMap[name] === undefined){
-    namesListedMap[name] = 1;
+    function reset() {
+        namesListedMap = {};
     }
-} 
 
-function allNames(){
-    return namesListedMap;
-}
+    function addMap(name) {
+        if (namesListedMap[name] === undefined){
+        namesListedMap[name] = 1;
+        }
+    } 
 
-return {
-    greet,
-    allNames,
- 
-};
+    function language(name, langItem) {
 
+        if (name !== "" && langItem !== "") {
+            if (langItem === "English") {
+                greeting = "Hello, " + name;
+            }
+            else if (langItem === "Afrikaans") {
+                greeting = "Halo, " + name;
+            }
+            else if (langItem === "isiXhosa") {
+                greeting = "Molo, " + name;
+            }
+        }
+        return greeting
+      }
+
+    function greetTotal() {
+        var greetTotal = Object.keys(namesListedMap)
+        return greetTotal.length;
+    }
+    
+    function regex(nameInput) {
+        var characters = /[^A-Za-z]/g;
+        if (nameInput !== "") {
+        var newName = nameInput.replace(characters, "")
+        return newName;
+     }
+    return "";
+    }
+
+    function allNames() {
+        return namesListedMap;
+    }
+
+    return {
+        reset,
+        addMap,
+        language,
+        greetTotal,
+        regex,
+        allNames,
+    }
 }
